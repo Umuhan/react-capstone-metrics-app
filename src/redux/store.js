c83d162f4592d8057a9251623a0cad67e253728a
Umu <hanny.ally123@gmail.com>
2023-11-23 19:31:54 +0300

    Add store.js

---
+++ src/redux/store.js
@@ -0,0 +1,11 @@
+import { configureStore } from "@reduxjs/toolkit";
+import countryReducer from "./CountryDetails/CountryDetails";
+import countriesReducer from "./Countries/CountriesSlice";
+
+const store = configureStore({
+  reducer: {
+    country: countryReducer,
+    countries: countriesReducer,
+  },
+});
+export default store;
