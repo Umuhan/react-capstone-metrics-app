f92c962836abda39736caba80c60cdf4c49f78ab
Umu <hanny.ally123@gmail.com>
2023-11-24 17:26:11 +0300

    Import index.css

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./index.css";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
